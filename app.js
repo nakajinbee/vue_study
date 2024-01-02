dad0f784348aa67943021a65c4dd0cc16a681c0b
nakajinbee <97015071+nakajinbee@users.noreply.github.com>
2024-01-02 17:01:27 +0900

    フォーマッター適応

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 Vue.createApp({
-    data: function(){
-        return{
-            todoTitle:'',
-            todoDescription:'',
-            todoCategories:[],
-            hideDoneTodo:false,
-            searchWord:'',
-            order:"desc"
-        }
+  data: function () {
+    return {
+      todoTitle: "",
+      todoDescription: "",
+      todoCategories: [],
+      hideDoneTodo: false,
+      searchWord: "",
+      order: "desc",
+    };
+  },
+  computed: {
+    canCreateTodo: function () {
+      return this.todoTitle !== "";
     },
-}).mount("#app")
+  },
+}).mount("#app");
