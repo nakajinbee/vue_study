f37d49d6ac4bb7e608583fdbf707a432aaaaf80f
nakajinbee <97015071+nakajinbee@users.noreply.github.com>
2024-01-02 17:21:52 +0900

    add form.

--- app.js
+++ app.js
@@ -7,11 +7,32 @@ Vue.createApp({
       hideDoneTodo: false,
       searchWord: "",
       order: "desc",
+      categoryName: "",
     };
   },
   computed: {
     canCreateTodo: function () {
       return this.todoTitle !== "";
+    },
+    canCreateCategory: function () {
+      return this.categoryName !== "";
+    },
+  },
+  methods: {
+    createTodo: function () {
+      if (!this.canCreateTodo) {
+        return;
+      }
+
+      this.todoTitle = "";
+      this.todoDescription = "";
+      this.todoCategories = [];
+    },
+    createCategory: function () {
+      if (!this.canCreateCategory) {
+        return;
+      }
+      this.categoryName = "";
     },
   },
 }).mount("#app");
